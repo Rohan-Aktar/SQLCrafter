bd7c21b537a459aa8421b61d084f7b601b879b6e
Dootam Roy <dootamroy22@gmail.com>
2024-03-01 20:35:01 +0530

    log4j LOG Generation.

--- src/main/java/Crafter/demo.java
+++ src/main/java/Crafter/demo.java
@@ -1,42 +1,47 @@
 package Crafter;
 
 import java.io.IOException;
+import java.io.PrintStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class demo
- */
+import Crafter.Log4jOutputStream;
+
 @WebServlet("/demo")
 public class demo extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void init() throws ServletException {
+        // Initialize Log4jOutputStream
+    	System.out.println("Initializing Log4j in servlet...");
+    	
+        Log4jOutputStream log4jOutputStream = new Log4jOutputStream();
+
+        // Set Log4jOutputStream as the standard output stream
+        System.setOut(new PrintStream(log4jOutputStream, true));
+    }
+
     public demo() {
         super();
-        // TODO Auto-generated constructor stub
         System.out.println("Maven build DEMO");
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+    	System.out.println("Inside doGet()");
+    	
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+        
+    }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
