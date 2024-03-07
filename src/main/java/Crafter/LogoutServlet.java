170be3ecaf1c1cbf96d39dcc00acb77b3733a663
Dootam Roy <dootamroy22@gmail.com>
2024-03-08 01:35:09 +0530

    log format update 🤫🤫

--- src/main/java/Crafter/LogoutServlet.java
+++ src/main/java/Crafter/LogoutServlet.java
@@ -1,6 +1,8 @@
 package Crafter;
 
 import java.io.IOException;
+import java.io.PrintStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +13,28 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    
+	public LogoutServlet() {
+        super();
+        
+        Log4jOutputStream log4jOutputStream = new Log4jOutputStream();
+        // Set Log4jOutputStream as the standard output stream
+        System.setOut(new PrintStream(log4jOutputStream, true));
+        
+        try {
+            System.out.println("LogoutServlet().. ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
+        
+        System.out.println("logging out user "+((UserModel)session.getAttribute("user")).getUsername()+"... ");
+        
         session.invalidate(); // Invalidate the session to log the user out
 
         response.sendRedirect(request.getContextPath() + "/home.jsp");
