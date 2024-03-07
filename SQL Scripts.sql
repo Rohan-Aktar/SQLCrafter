cd88b82f200a7b338d55d3db8e95a18318afb3d9
Dootam Roy <dootamroy22@gmail.com>
2024-03-07 23:49:01 +0530

    Create SQL Scripts.sql

---
+++ SQL Scripts.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS Crafter;
+
+
+CREATE TABLE  Crafter.users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+insert into Crafter.users (username, password,email)values('DOOTAM','Dootam123','xyz@gmail.com');
+
+select * from Crafter.users;
+
+
+
