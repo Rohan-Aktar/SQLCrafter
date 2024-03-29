8b722b11f051d46c99fcf271b01c473f379c505a
Dootam Roy <dootamroy22@gmail.com>
2024-03-29 22:21:59 +0530

    Update SQL_SCRIPTS.sql

--- SQL_SCRIPTS.sql
+++ SQL_SCRIPTS.sql
@@ -80,3 +80,30 @@ insert into Crafter.t_crafter_role_access (role_id,
                                             'A'
                                         ); --for admin role id =1;
 select * from Crafter.t_crafter_role_access;
+
+
+CREATE TABLE  Crafter.t_crafter_database (
+    database_id INT AUTO_INCREMENT PRIMARY KEY,
+    database_username VARCHAR(50) NOT NULL,
+    database_password VARCHAR(255) NOT NULL,
+    database_url VARCHAR(50) NOT NULL,
+    database_schema VARCHAR(50),
+    creation_date date,
+    updation_date date,
+    status varchar(1) DEFAULT('A')
+);
+--drop table Crafter.t_crafter_database;
+
+insert into Crafter.t_crafter_database(database_username ,
+                                    database_password ,
+                                    database_url ,
+                                    database_schema,
+                                    creation_date)
+                                     VALUES(
+                                        'root',
+                                        'rocketman123',
+                                        'mysql://localhost:3306',
+                                        'Crafter',
+                                        SYSDATE()
+                                    );
+select * from Crafter.t_crafter_database;
